eda440c9326ab367ee52658024785a3699e7943d
Rupesh Jaiswar <52023261+rupeshjaiswar@users.noreply.github.com>
2021-01-04 23:06:47 +0530

    Add files via upload

---
+++ webdevlopment-learning/HTML5-CSS3/dist/calculator.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+var disp = "9 + 6 + 7 - 8";
+dis = _toConsumableArray(disp);
+dis.forEach(function (element, index) {
+  if (element == "+") {
+    console.log(element[index - 1] + element[index + 1]);
+  }
+});
